f78292b001bd323bd227d7e7f9755abc5d54acf0
Ankit Anand <ankitanandraj9563@gmail.com>
2026-03-01 10:34:00 +0530

    Add files via upload

---
+++ Account-Management/src/main/java/com/accmang/AccManagement/AccountController.java
@@ -0,0 +1,40 @@
+package com.accmang.AccManagement;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userAcc")
+public class AccountController {
+
+    @GetMapping("/management")
+    public String test() {
+        return "Account Service Running";
+    }
+
+    @GetMapping("/testing")
+    public String test1() {
+        return "Service Working";
+    }
+
+    @GetMapping("/details")
+    public String details() {
+        return "Account Service Working";
+    }
+
+
+    @GetMapping("/detailss")
+    public String details1() {
+        return "Account Service Working...!!";
+    }
+
+
+    @GetMapping("/detailsss")
+    public String details2() {
+        return "Account Service Working !!";
+    }
+}
+
+
+
---
+++ Account-Management/src/test/java/com/accmang/AccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accmang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
